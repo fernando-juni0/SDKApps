55cffa94334a606203f8ffa52f5e932b97e3ae90
Fernando Júnio <junio132sj@gmail.com>
2024-03-08 13:25:33 -0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 
 var url = encodeURIComponent(origin + '/auth/callback')
-var novaUrl = `https://discord.com/oauth2/authorize?client_id=1210894508028338197&response_type=code&redirect_uri=https%3A%2F%2Falert-ghastly-lizard.ngrok-free.app%2Fauth%2Fcallback&scope=identify+guilds+email`;
+var novaUrl = `https://discord.com/oauth2/authorize?client_id=1210894508028338197&response_type=code&redirect_uri=http%3A%2F%2Ffernandojunio.com.br%3A4141%2Fauth%2Fcallback&scope=identify+guilds+email`;
 
 
 document.querySelectorAll('.button-assinar').forEach(element=>{
